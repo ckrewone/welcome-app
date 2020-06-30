7c2b3fe1cfc1066c4c44499e9f69672f99947ce9
Michał Żakowski <michal@MacBook-Pro-Micha.local>
2020-06-30 21:31:03 +0200

    Update icons

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PageList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageList from './components/PageList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PageList
   }
 }
 </script>
