cae1bc7b8f668ee995e4a895e75c41a125652c8d
Michał Żakowski <michal@MacBook-Pro-Micha.local>
2020-07-10 19:10:09 +0200

    use proxy which resolve problem with cors

--- src/components/Wallpapers/useFetchWallpapers.js
+++ src/components/Wallpapers/useFetchWallpapers.js
@@ -6,7 +6,7 @@ export default function useFetchWallpaper() {
     const { fetch } = useFetch();
     const config = {
         wallpapers: {
-            url : "https://wallhaven.cc/api/v1/",
+            url : "https://cors-anywhere.herokuapp.com/https://wallhaven.cc/api/v1/",
             apiKey: "nOhRnwoazI2R4KhMGrImVaAsYke6nfIe"
         }
     };
